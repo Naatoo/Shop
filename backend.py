6d228dea036f0a94c5f7c503b50ca41337bb19b7
natooo <thenatooorat@gmail.com>
2018-01-06 03:48:41 +0100

    Add view products option

---
+++ backend.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+
+def view(table_name):
+    data = (table_name,)
+    connection = psycopg2.connect("dbname='shop' user='postgres' password='natoo123' host='localhost' port='5432'")
+    cursor = connection.cursor()
+
+    sql = "SELECT * FROM %s" % table_name
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+
+    sql = "SELECT column_name FROM information_schema.columns WHERE table_name=%s"
+    cursor.execute(sql, data)
+    column_names = cursor.fetchall()
+    column_names_final = [tup[0] for tup in column_names]
+    print(column_names_final)
+
+    connection.close()
+    return column_names_final, rows
